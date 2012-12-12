c3283d0babba8680669dd061d1b63dd4017a0395
Gabriel Delattre <gabriel@soixantecircuits.fr>
2012-12-12 01:07:17 +0100

    adding simple fields

--- wp-content/themes/responsive/landing-page.php
+++ wp-content/themes/responsive/landing-page.php
@@ -44,7 +44,11 @@ if ( !defined('ABSPATH')) exit;
             
             <div class="post-edit"><?php edit_post_link(__('Edit', 'responsive')); ?></div> 
             </div><!-- end of #post-<?php the_ID(); ?> -->
-                        
+            <?php
+            $selected_values = simple_fields_get_post_group_values(get_the_id(), "Blocks", true, 1);
+            print_r($selected_values);
+            ?>
+            
         <?php endwhile; ?> 
         
         <?php if (  $wp_query->max_num_pages > 1 ) : ?>
